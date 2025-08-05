b1f7fcce0d6e3277ea06d9f5ef6282c6acd03a91
Dorus <doruskeijzer@gmail.com>
2025-08-05 15:29:57 +0200

    feat: basic crawler setup

--- scrapers/crawler.py
+++ scrapers/crawler.py
@@ -1,22 +1,47 @@
-
-
 from playwright.sync_api import sync_playwright
 from parsel import Selector
+import uuid
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,  
+    format='[%(asctime)s] [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
 
 class Scraper:
     """Takes the name of an area and returns all available listings in the area"""
-    def __init__(self, area):
+    def __init__(self, area: str):
         self.area = area
+        self.base_url = f"https://www.funda.nl/zoeken/koop/?selected_area=[\"{area.lower()}\"]&search_result="
+        self.links = []
+        self.name= f"Scraper-{area}-{uuid.uuid4().hex[:6]}"
+        self.logger = logging.getLogger(self.name)
+        self.logger.info("Initialized.")
 
-    def scrape(self, url):
-        with sync_playwright() as p:
-            browser = p.chromium.launch(headless=False)
-            page = browser.new_page()
-            page.goto(url)            
-            page.wait_for_load_state("networkidle")
-            content = page.content()
-            selector = Selector(text = content)
-            title = selector.css("title").get()
-            print(title)
-            browser.close()
+    def scrape_links(self):
+        page_number = 1
 
+        while True:
+            self.logger.info(f"Crawling page {page_number}")
+            url = self.base_url + str(page_number)
+
+            with sync_playwright() as p:
+                self.logger.info(f"Making request to {url}...")
+                browser = p.chromium.launch(headless=False)
+                page = browser.new_page()
+                page.goto(url)            
+                page.wait_for_load_state("networkidle")
+                content = page.content()
+                selector = Selector(text = content)
+                urls = selector.css("a").getall()
+                self.logger.info(f"Found {len(urls)} urls.")
+                browser.close()
+
+            page_number += 1
+            quit()
+
+if __name__ == "__main__":
+    scraper = Scraper("Tilburg")
+    scraper.scrape_links()
